bcf3fd458508b5be9d08fbdf5e4cda83bc018ea6
Heidi I. Chen 陳濬怡 <hchen17@stanford.edu>
2022-10-04 16:28:36 -0700

    Update generateLookupFile.sh
    switch ASM223467v1_ensembl98_canonicalTSS.bed to ASM223467v1_ensembl98_canonicalTSS_withInfo.tab

--- scripts/generateLookupFile.sh
+++ scripts/generateLookupFile.sh
@@ -3,14 +3,14 @@
 # This script generates an ortho chain "lookup file" for each canonical gene and any aligning species to which the gene has been mapped
 
 # referenceGeneTSSs:
-# chr1	1604082	1604083	ENSORLG00000000001
+# chr1	58832	58833	[no name]	ENSORLG00000025847	ENSORLT00000035921	[no description]
 # ...
 
 set -beEuo pipefail
 
 scriptsDir=$(realpath $(dirname $0))
 
-gene=$1 # e.g. ENSORLG00000000001
+gene=$1 # e.g. ENSORLG00000025847
 
 num=$(echo $gene | sed 's/ENSORLG//' | sed "s/^0\+//")
 folderPrefix=$(($num/1000))
@@ -24,7 +24,7 @@ mkdir -p ${outroot}/${folderPrefix}
 
 outpath=${outroot}/${folderPrefix}/$gene
 
-referenceGeneTSSs=${scriptsDir}/../processedInputs/filterBEDs/ASM223467v1_ensembl98_canonicalTSS.bed
+referenceGeneTSSs=${scriptsDir}/../processedInputs/filterBEDs/ASM223467v1_ensembl98_canonicalTSS_withInfo.tab
 
 grep $gene $referenceGeneTSSs | awk '{print $1 "\t" $2 "\t" $3}' > $outpath
 results=$(grep "$gene" ${inroot}/*reformattedChainIds)
