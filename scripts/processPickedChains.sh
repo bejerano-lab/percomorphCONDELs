7771f3f286b88543534c07ac32412c21e8665b5d
Heidi I. Chen 陳濬怡 <hchen17@stanford.edu>
2022-10-04 12:52:38 -0700

    Update processPickedChains.sh

--- scripts/processPickedChains.sh
+++ scripts/processPickedChains.sh
@@ -2,14 +2,14 @@
 
 set -beuEo pipefail
 
-species=$(basename $1 .out)
+species=$1
 scriptsDir=$(dirname $0)
 setupOutDir=${scriptsDir}/../pickOrthoChains/
 orthoChainOutDir=${scriptsDir}/../processedInputs/orthoChains/
 canonicalIDs=${scriptsDir}/../pickOrthoChains/ensembl98_ASM223467v1_canonical_IDs.txt
 
 # REFORMAT PICK CHAIN OUTPUT
-join <(grep ^ENSORLG $1 | cut -d" " -f1,2 | sort) ${canonicalIDs} > ${setupOutDir}/${species}".chain_ids"
+join <(grep ^ENSORLG ${setupOutDir}/${1}.out | cut -d" " -f1,2 | sort) ${canonicalIDs} > ${setupOutDir}/${species}".chain_ids"
 
 awk -v var=$species '{print $1 "\t" var"_chain"$2}'  ${setupOutDir}/${species}".chain_ids" > ${setupOutDir}/${species}_reformattedChainIds
 
