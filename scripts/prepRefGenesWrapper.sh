227c9f561a7e4038c29e7df74cbb4a66e98afd95
Heidi I. Chen <hchen17@stanford.edu>
2022-10-04 20:24:34 -0700

    initial commit of prepRefGenesWrapper.sh

---
+++ scripts/prepRefGenesWrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -beEuo pipefail
+
+scriptsDir=$(realpath $(dirname $0))
+
+ensemblBiomartFile=$(readlink -f $1)
+ensemblGTFgz=$(readlink -f $2)
+
+python3 ${scriptsDir}/prepRefGeneInfo.py ${ensemblBiomartFile}
+
+## identify introns ##
+
+# first make a gff3 file of exons from ensembl's gtf
+
+zcat ${ensemblGTFgz} | awk '$3 == "exon"' > /tmp/exonsOnly.gtf
+gt gtf_to_gff3 -force -o /tmp/exonsOnly.gff3 /tmp/exonsOnly.gtf
+gt gff3 -force yes  -addintrons yes -sort yes -o /tmp/exonsAndIntrons.gff3 /tmp/exonsOnly.gff3
+
